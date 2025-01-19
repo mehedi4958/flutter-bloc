4cc34e2a06b52854864dfdd9d93dab62f34da0d8
Mehedi Hasan <mehedi4958@gmail.com>
2025-01-19 23:26:13 +0600

    sign in bloc state created

---
+++ lib/pages/sign_in/bloc/sign_in_states.dart
@@ -0,0 +1,15 @@
+class SignInState {
+  SignInState({
+    this.email = '',
+    this.password = '',
+  });
+  final String email;
+  final String password;
+
+  SignInState copyWith({String? email, String? password}) {
+    return SignInState(
+      email: email ?? this.email,
+      password: password ?? this.password,
+    );
+  }
+}
