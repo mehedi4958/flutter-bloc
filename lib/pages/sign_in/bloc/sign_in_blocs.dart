a28cd67970cee402c0505d5bea5e5e05f72fe679
Mehedi Hasan <mehedi4958@gmail.com>
2025-01-19 23:49:57 +0600

    sign in bloc class created

---
+++ lib/pages/sign_in/bloc/sign_in_blocs.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_block/pages/sign_in/bloc/sign_in_events.dart';
+import 'package:flutter_block/pages/sign_in/bloc/sign_in_states.dart';
+
+class SignInBloc extends Bloc<SignInEvents, SignInStates> {
+  SignInBloc() : super(SignInStates()) {
+    on<EmailEvent>(_emailEvent);
+    on<PasswordEvent>(_passwordEvent);
+  }
+
+  void _emailEvent(EmailEvent event, Emitter<SignInStates> emit) {
+    emit(state.copyWith(email: event.email));
+  }
+
+  void _passwordEvent(PasswordEvent event, Emitter<SignInStates> emit) {
+    emit(state.copyWith(password: event.password));
+  }
+}
