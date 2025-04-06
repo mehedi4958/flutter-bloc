b176dfe2ed81bcb5bf3fedf1a5dfb5e48cab9f90
Mehedi Hasan <mehedi4958@gmail.com>
2025-04-06 08:46:22 +0800

    register states class completed

--- lib/pages/register/bloc/register_states.dart
+++ lib/pages/register/bloc/register_states.dart
@@ -1,7 +1,22 @@
 class RegisterStates {
-  RegisterStates(this.userName, this.email, this.password, this.rePassword);
-  String userName;
-  String email;
-  String password;
-  String rePassword;
+  const RegisterStates(
+      this.userName, this.email, this.password, this.rePassword);
+  final String userName;
+  final String email;
+  final String password;
+  final String rePassword;
+
+  RegisterStates copyWith({
+    String? userName,
+    String? email,
+    String? password,
+    String? rePassword,
+  }) {
+    return RegisterStates(
+      userName ?? this.userName,
+      email ?? this.email,
+      password ?? this.password,
+      rePassword ?? this.rePassword,
+    );
+  }
 }
